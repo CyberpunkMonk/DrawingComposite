2de44885bd3c6019e5c5afeb219db9dc4c8de5d3
Connor Boesch <Connor_Boesch@yahoo.com>
2016-04-23 22:22:44 -0500

    Commit code to repo.

---
+++ DrawingComposite/PrimitiveElement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace DrawingComposite {
+	class PrimitiveElement:DrawingElement {
+		//C'tor
+		public PrimitiveElement(string name):base(name){}
+		public override void Add(DrawingElement d) {
+			Console.WriteLine("Cannot add to a PrimitiveElement");
+		}
+
+		public override void Remove(DrawingElement d) {
+			Console.WriteLine("Cannot remove from a PrimitiveElement");
+		}
+
+		public override void Display(int indent) {
+			Console.WriteLine(new String('-',indent)+" "+_name);
+		}
+	}
+}
